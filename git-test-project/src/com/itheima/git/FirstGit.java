8af7912950e23403847a8f7ff67dd526138b08c9
FengSenHua <1146977578@qq.com>
2019-07-25 19:12:07 +0800

    把本地工程添加到本地仓库

---
+++ git-test-project/src/com/itheima/git/FirstGit.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
